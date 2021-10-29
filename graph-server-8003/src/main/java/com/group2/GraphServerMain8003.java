7f659c2f3e5f89602b7958c190f4d84679c90f63
shichenhang <181250124@smail.nju.edu.cn>
2021-10-29 19:09:37 +0800

    fix: 使用idea直接运行时无法从neo4j获得数据

--- graph-server-8003/src/main/java/com/group2/GraphServerMain8003.java
+++ graph-server-8003/src/main/java/com/group2/GraphServerMain8003.java
@@ -2,14 +2,10 @@ package com.group2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableNeo4jRepositories("com.group2.repository")
-//@EntityScan(basePackages = "com.group2.entity")
 public class GraphServerMain8003 {
     public static void main(String[] args) {
         SpringApplication.run(GraphServerMain8003.class,args);
